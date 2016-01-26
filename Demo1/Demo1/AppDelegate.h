082a492e280745793ea988a45452376e1a9203dd
“jameswatt” <jameswatt2008@gmail.com>
2016-01-26 18:03:39 +0800

    1

---
+++ Demo1/Demo1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Demo1
+//
+//  Created by jameswatt on 16/1/26.
+//  Copyright © 2016年 xuzhixiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Demo1/Demo1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Demo1
+//
+//  Created by jameswatt on 16/1/26.
+//  Copyright © 2016年 xuzhixiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
